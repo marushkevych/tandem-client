8caec8190ec8b5b71b575234f8445aae615c18d5
marushkevych <marushkevych@gmail.com>
2014-12-29 11:48:36 -0500

    repackaging server and client

--- test/runServer.js
+++ test/runServer.js
@@ -1,4 +1,5 @@
-var server = require('../index').createServer();
+var listener = require('../index');
+var server = listener.createServer();
 
 server.listen(18888, function() { //'listening' listener
     console.log('server bound');
